640eae555b56f095e84f9e8d3621c4db1dd2585d
kaiomedau <k.medau@gmail.com>
2016-01-02 00:59:46 -0200

    Add image width and height to response

--- server/index.php
+++ server/index.php
@@ -14,7 +14,9 @@ $response   = file_get_contents($finalAPIUrl);
 $json       = json_decode($response);
 
 $imgSRC     = $json->{'data'}->{'image_original_url'};
+$imgWidth   = $json->{'data'}->{'image_width'};
+$imgHeight  = $json->{'data'}->{'image_height'};
 
-echo json_encode( array("data" => array( "url"=> $imgSRC ) ) );
+echo json_encode( array("data" => array( "url"=> $imgSRC , "width" => $imgWidth, "height" => $imgHeight) ) );
 
 ?>
